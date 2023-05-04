3deae689234e266b6d26a42b79b0056367ce37e4
Usama Husnain <usama.husnain@nexarise.com>
2023-05-04 20:40:29 +0500

    fee module done

--- resources/views/admin/fee/feeSlip.blade.php
+++ resources/views/admin/fee/feeSlip.blade.php
@@ -26,60 +26,67 @@
         <hr />
         <div id="printarea">
             @for($i=0; $i<=1; $i++)
-            <div class="row">
 
-                    <h2 class=" fw-bolder text-center ">IDEAL ACADEMY</h2>
-                    <h3 class="mb-1 fw-bold text-center">Fee Slip</h3>
-                    <h5 class="mb-0 fw-bold text-center border border-dark rounded m-auto p-2  w-50" ><span class="small">(For the Month of {{ now()->format('F Y') }})</span> </h5>
+            <div class="row {{($i==1) ? "mt-4" : ""}}">
+
+                    <h2 class=" mb-0 fw-bolder text-center ">IDEAL ACADEMY</h2>
+                    <h3 class="mb-0 fw-bold text-center">Fee Slip</h3>
+                    {{-- <h5 class="mb-0 fw-bold text-center border border-dark rounded m-auto p-2  w-50" ><span class="small">(For the Month of {{ now()->format('F Y') }})</span> </h5> --}}
             </div>
-            <hr/>
 
-            <h5 class="text-center mt-5 fw-bold my-3 text-decoration-underline">Slip For {{ ($i==0) ? "Academy" : "Student"}}</h5>
-            <div class="row ">
-                <div class="col-6 text-center ">
-                    <label class="fw-bold">Student Name:</label>
-                    {{$fee->student->fname.' '.$fee->student->lname}}
-                </div>
-                <div class="col-6 text-center ">
-                    <label class="fw-bold">Roll #:</label>
-                    {{$fee->student->roll}}
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6 text-center ">
-                    <label class="fw-bold">Monthly Fee:</label>
-                    {{$fee->student->fee." /-"}}
-                </div>
-                <div class="col-6 text-center ">
-                    <label class="fw-bold">Class:</label>
-                    {{$fee->student->class}}
-                </div>
+            <table class="table table-bordered mt-1">
+                <thead class="bg-light">
+                    <tr>
+                        <th  colspan="2">
+                            <h5 class="text-center fw-bold  text-decoration-underline">Slip For {{ ($i==0) ? "Academy" : "Student"}}</h5>
+                        </th>
+                    </tr>
+                </thead>
+                <tr>
+                    <th>Student Name</th>
+                    <td>
+                        {{$fee->student->fname.' '.$fee->student->lname}}
+                    </td>
+                </tr>
+                <tr>
+                    <th>Roll #</th>
+                    <td>
+                        {{$fee->student->roll}}
+                    </td>
+                </tr>
+                <tr>
+                    <th>Class</th>
+                    <td>
+                        {{$fee->student->class}}
+                    </td>
+                </tr>
+                <tr>
+                    <th>Monthly Fee</th>
+                    <td>
+                        {{$fee->student->fee.' /-'}}
+                    </td>
+                </tr>
+                <tr>
+                    <th>Date</th>
+                    <td>
+                        {{date('d-m-Y',strtotime($fee->created_at))}}
+                    </td>
+                </tr>
+                <tr>
+                    <th>Fee Paid</th>
+                    <td>
+                        {{$fee->paid_fee.' /- '}} <span class="badge  badge-success">{{$fee->status}}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Remarks</th>
+                    <td>
+                        <?php echo $fee->remarks ?>
+                    </td>
+                </tr>
 
-            </div>
-            <div class="row">
-                <div class="col-6 text-center ">
-                    <label class="fw-bold">Date:</label>
-                    {{date('d-m-Y',strtotime($fee->created_at))}}
-                </div>
+            </table>
 
-                <div class="col-6 text-center ">
-                    <label class="fw-bold">  Fee Paid:</label>
-                    {{$fee->paid_fee." /-"}} <span class="badge  badge-success">{{$fee->status}}</span>
-                </div>
-
-            </div>
-            <div class="row">
-                <div class="col-6 text-center ">
-                    <label class="fw-bold">Remarks:</label>
-                    <?php echo $fee->remarks ?>
-                </div>
-                <div class="col-6 text-center ">
-
-                </div>
-            </div>
-            <div class="p mb-5 text-center ">
-
-            </div>
 
 
             @endfor
